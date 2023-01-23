edb588badf022ef88f6a7fdeee78eda8607bc168
Eduardo Ohe <eduohe@users.noreply.github.com>
2023-01-22 18:49:13 -0800

    Renamed coin_events to fungible_token_events and added partition definitions and migration cmds

--- indexer-events/migrations/20230120134210_add_partitions.sql
+++ indexer-events/migrations/20230120134210_add_partitions.sql
@@ -7,14 +7,39 @@ SELECT min(event_index), max(event_index) FROM coin_events;
 -- min: 16136043940348625390000000008000000,              max 16742378612931940910000003001000005 
 --      16136043940000000000000000000000000 (Feb 17 2021)     16742378610000000000000000000000000 (Jan 20, 2023)
 -- This table has 329 GB, this columns has epoch timestamp as prefix
--- We have almost 2 years of data, we can try to split the data in partitions of 6 months
--- 16094592000000000000000000000000000 (2021-01-01) [p1] - OK (Old)
--- 16250976000000000000000000000000000 (2021-07-01) [p2] - Later
--- 16409952000000000000000000000000000 (2022-01-01) [p3] - Later
--- 16566336000000000000000000000000000 (2022-07-01) [p4] - Later
--- 16725312000000000000000000000000000 (2023-01-01) [p5] - OK
--- 16881696000000000000000000000000000 (2023-07-01) [p6] - OK
--- 17040672000000000000000000000000000 (2024-01-01) [p7] - Later
+-- We have almost 2 years of data, we can try to split the data in partitions by months
+
+-- 16094592000000000000000000000000000 (2021-01-01)
+-- 16121376000000000000000000000000000 (2021-02-01) 
+-- 16145568000000000000000000000000000 (2021-03-01) 
+-- 16172352000000000000000000000000000 (2021-04-01) 
+-- 16198272000000000000000000000000000 (2021-05-01) 
+-- 16225056000000000000000000000000000 (2021-06-01) 
+-- 16250976000000000000000000000000000 (2021-07-01)
+-- 16277760000000000000000000000000000 (2021-08-01)
+-- 16304544000000000000000000000000000 (2021-09-01)
+-- 16330464000000000000000000000000000 (2021-10-01) 
+-- 16357248000000000000000000000000000 (2021-11-01) 
+-- 16383168000000000000000000000000000 (2021-12-01)
+
+-- 16409952000000000000000000000000000 (2022-01-01)
+-- 16436736000000000000000000000000000 (2022-02-01) 
+-- 16460928000000000000000000000000000 (2022-03-01) 
+-- 16487712000000000000000000000000000 (2022-04-01) 
+-- 16513632000000000000000000000000000 (2022-05-01) 
+-- 16540416000000000000000000000000000 (2022-06-01) 
+-- 16566336000000000000000000000000000 (2022-07-01)
+-- 16593120000000000000000000000000000 (2022-08-01)
+-- 16619904000000000000000000000000000 (2022-09-01)
+-- 16645824000000000000000000000000000 (2022-10-01) 
+-- 16672608000000000000000000000000000 (2022-11-01) 
+-- 16698528000000000000000000000000000 (2022-12-01) 
+
+-- 16094592000000000000000000000000000 (2021-01-01 to 2021-01-01) - OK (Old)
+-- 16725312000000000000000000000000000 (2023-01-01 to 2023-02-01) - OK
+-- 16752096000000000000000000000000000 (2023-02-0 to 2023-03-01) - OK
+-- 16776288000000000000000000000000000 (2023-03-01 to 2023-04-01) 
+
 -- NOTE: Team suggested to use a new table name fungible_token_events instead of coin_events
 
 BEGIN TRANSACTION;
@@ -49,40 +74,48 @@ CREATE INDEX fungible_token_events_affected_account_id_idx ON public.fungible_to
 CREATE INDEX fungible_token_events_block_height_idx ON public.fungible_token_events USING btree (block_height);
 CREATE INDEX fungible_token_events_receipt_id_idx ON public.fungible_token_events USING btree (receipt_id);
 CREATE INDEX fungible_token_events_block_timestamp_idx ON fungible_token_events USING btree (block_timestamp);
--- Create a partition to start after the MAX found (2023-07-01 to 2024-01-01)
-CREATE TABLE fungible_token_events_p202307 PARTITION OF fungible_token_events FOR VALUES FROM (16881696000000000000000000000000000) TO (17040672000000000000000000000000000);
+-- Create a partition to start after next month (2023-02-01 to 2023-03-01)
+CREATE TABLE fungible_token_events_p202302 PARTITION OF fungible_token_events FOR VALUES FROM (16752096000000000000000000000000000) TO (16776288000000000000000000000000000);
 
 COMMIT;
 
--- Create a check constraint in the old table (2021-01-01 to 2023-07-01)
-ALTER TABLE fungible_token_events_old ADD CONSTRAINT fungible_token_events_old_check_constraint CHECK (event_index >= 16094592000000000000000000000000000 AND event_index < 16881696000000000000000000000000000) NOT VALID;
+-- Create a check constraint in the old table (2021-01-01 to 2023-02-01)
+ALTER TABLE fungible_token_events_old ADD CONSTRAINT fungible_token_events_old_check_constraint CHECK (event_index >= 16094592000000000000000000000000000 AND event_index < 16752096000000000000000000000000000) NOT VALID;
 
--- Attach the old table as partition with previous data (2021-01-01 to 2023-07-01)
-ALTER TABLE fungible_token_events ATTACH PARTITION fungible_token_events_old FOR VALUES FROM (16094592000000000000000000000000000) TO (16881696000000000000000000000000000);
+-- Attach the old table as partition with previous data (2021-01-01 to 2023-02-01)
+ALTER TABLE fungible_token_events ATTACH PARTITION fungible_token_events_old FOR VALUES FROM (16094592000000000000000000000000000) TO (16752096000000000000000000000000000);
 
 -- Now we can drop the old table constraint 
 ALTER TABLE fungible_token_events_old DROP CONSTRAINT fungible_token_events_old_check_constraint;
 -- DONE!!! At this point the table is usable again :)
 
 
+CREATE OR REPLACE FUNCTION fn_partition_by_range(_tbl text, _poldname text, _pnewname text, _pkey text, _start numeric, _end numeric)
+  RETURNS void
+  LANGUAGE plpgsql AS
+$func$
+BEGIN
+	-- Detach old partition
+	EXECUTE 'ALTER TABLE ' || _tbl || ' DETACH PARTITION ' || _poldname;
+	-- Create partition from _start to _end
+	EXECUTE 'CREATE TABLE ' || _pnewname || ' PARTITION OF ' || _tbl || ' FOR VALUES FROM (' || _start || ') TO (' || _end || ')';
+	-- Insert data to partition from _start to _end
+	EXECUTE 'INSERT INTO ' || _pnewname || ' SELECT * FROM ' || _poldname || ' WHERE ' || _pkey || ' >= ' || _start || ' AND ' || _pkey || ' < ' || _end;
+	-- Delete data from old partition from _start to _end
+	EXECUTE 'DELETE FROM ' || _poldname || ' WHERE ' || _pkey || ' >= ' || _start || ' AND ' || _pkey || ' < ' || _end;
+	-- Add check on old partition from 0 to _start
+	EXECUTE 'ALTER TABLE ' || _poldname || ' ADD CONSTRAINT ' || _poldname || '_check_constraint CHECK (' || _pkey || ' >= 0 AND ' || _pkey || ' < ' || _start || ') NOT VALID';
+	-- Re-Attach old partition from 0 to _start
+	EXECUTE 'ALTER TABLE ' || _tbl || ' ATTACH PARTITION ' || _poldname || ' FOR VALUES FROM (0) TO (' || _start || ')';
+	-- Drop the check on old partition
+	EXECUTE 'ALTER TABLE ' || _poldname || ' DROP CONSTRAINT ' || _poldname || '_check_constraint';
+END
+$func$;
 
--- To move data (break it down) from the large partition to smaller ones:
-BEGIN TRANSACTION;
--- Detach old partition
-ALTER TABLE fungible_token_events DETACH PARTITION fungible_token_events_old;
--- Create partition from 2023-01-01 to 2023-07-01
-CREATE TABLE fungible_token_events_p202301 PARTITION OF fungible_token_events FOR VALUES FROM (16725312000000000000000000000000000) TO (16881696000000000000000000000000000);
--- Insert data to partition from 2023-01-01 to 2023-07-01
-INSERT INTO fungible_token_events_p202301 SELECT * FROM fungible_token_events_old WHERE event_index >= 16725312000000000000000000000000000 AND event_index < 16881696000000000000000000000000000;
--- Delete data from old partition from 2023-01-01 to 2023-07-01
-DELETE FROM fungible_token_events_old WHERE event_index >= 16725312000000000000000000000000000 AND event_index < 16881696000000000000000000000000000;
--- Add check on old partition from 2021-01-01 to 2023-01-01
-ALTER TABLE fungible_token_events_old ADD CONSTRAINT fungible_token_events_old_check_constraint CHECK (event_index >= 16094592000000000000000000000000000 AND event_index < 16725312000000000000000000000000000) NOT VALID;
--- Re-Attach old partition from 2021-01-01 to 2023-01-01
-ALTER TABLE fungible_token_events ATTACH PARTITION fungible_token_events_old FOR VALUES FROM (16094592000000000000000000000000000) TO (16725312000000000000000000000000000);
--- Drop the check on old partition
-ALTER TABLE fungible_token_events_old DROP CONSTRAINT fungible_token_events_old_check_constraint;
-COMMIT;
+-- 2023-01-01 to 2023-02-01
+SELECT * FROM fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202301', 'event_index', 16698528000000000000000000000000000, 16881696000000000000000000000000000);
+-- 2022-12-01 to 2023-01-01
+SELECT * FROM fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202212', 'event_index', 16698528000000000000000000000000000, 16725312000000000000000000000000000);
 
 -- Vacuum the table
 VACUUM fungible_token_events;
