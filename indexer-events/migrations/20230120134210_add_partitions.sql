3593d782d623fc5f1141b8e5742f381973f74e48
Eduardo Ohe <eduohe@users.noreply.github.com>
2023-01-24 09:56:06 -0800

    Renamed coin_events to fungible_token_events and added partition definitions and migration cmds

--- indexer-events/migrations/20230120134210_add_partitions.sql
+++ indexer-events/migrations/20230120134210_add_partitions.sql
@@ -8,40 +8,17 @@ SELECT min(event_index), max(event_index) FROM coin_events;
 --      16136043940000000000000000000000000 (Feb 17 2021)     16742378610000000000000000000000000 (Jan 20, 2023)
 -- This table has 329 GB, this columns has epoch timestamp as prefix
 -- We have almost 2 years of data, we can try to split the data in partitions by months
-
--- 16094592000000000000000000000000000 (2021-01-01)
--- 16121376000000000000000000000000000 (2021-02-01) 
--- 16145568000000000000000000000000000 (2021-03-01) 
--- 16172352000000000000000000000000000 (2021-04-01) 
--- 16198272000000000000000000000000000 (2021-05-01) 
--- 16225056000000000000000000000000000 (2021-06-01) 
--- 16250976000000000000000000000000000 (2021-07-01)
--- 16277760000000000000000000000000000 (2021-08-01)
--- 16304544000000000000000000000000000 (2021-09-01)
--- 16330464000000000000000000000000000 (2021-10-01) 
--- 16357248000000000000000000000000000 (2021-11-01) 
--- 16383168000000000000000000000000000 (2021-12-01)
-
--- 16409952000000000000000000000000000 (2022-01-01)
--- 16436736000000000000000000000000000 (2022-02-01) 
--- 16460928000000000000000000000000000 (2022-03-01) 
--- 16487712000000000000000000000000000 (2022-04-01) 
--- 16513632000000000000000000000000000 (2022-05-01) 
--- 16540416000000000000000000000000000 (2022-06-01) 
--- 16566336000000000000000000000000000 (2022-07-01)
--- 16593120000000000000000000000000000 (2022-08-01)
--- 16619904000000000000000000000000000 (2022-09-01)
--- 16645824000000000000000000000000000 (2022-10-01) 
--- 16672608000000000000000000000000000 (2022-11-01) - OK
--- 16698528000000000000000000000000000 (2022-12-01) - OK
--- 16698528000000002000000000000000000
-
--- 16094592000000000000000000000000000 (2021-01-01 to 2021-01-01) - OK (Old)
--- 16725312000000000000000000000000000 (2023-01-01 to 2023-02-01) - OK
--- 16752096000000000000000000000000000 (2023-02-01 to 2023-03-01) - OK
--- 16776288000000000000000000000000000 (2023-03-01 to 2023-04-01) 
-
 -- NOTE: Team suggested to use a new table name fungible_token_events instead of coin_events
+
+-- Support function to convert timestamo to numeric 38 nanosec
+CREATE OR REPLACE FUNCTION fn_timestamp2nanosec(_ptimestamp TIMESTAMP)
+  RETURNS numeric(38, 0)
+  LANGUAGE plpgsql AS
+$func$
+BEGIN
+	RETURN (EXTRACT(EPOCH FROM (_ptimestamp))::numeric(38, 0) * pow(10,25)::numeric(38, 0));
+END
+$func$;
 
 BEGIN TRANSACTION;
 -- Rename table and indexes to <table/index>_old
@@ -78,15 +55,15 @@ CREATE INDEX fungible_token_events_block_timestamp_idx ON public.fungible_token_
 CREATE INDEX fungible_token_events_affected_account_id_idx ON public.fungible_token_events USING btree (affected_account_id);
 CREATE INDEX fungible_token_events_contract_account_id_idx ON public.fungible_token_events USING btree (contract_account_id);
 -- Create a partition to start after next month (2023-02-01 to 2023-03-01)
-CREATE TABLE fungible_token_events_p202302 PARTITION OF fungible_token_events FOR VALUES FROM (16752096000000000000000000000000000) TO (16776288000000000000000000000000000);
+CREATE TABLE fungible_token_events_p202302 PARTITION OF fungible_token_events FOR VALUES FROM (fn_timestamp2nanosec(TIMESTAMP '2023-02-01')) TO (fn_timestamp2nanosec(TIMESTAMP '2023-03-01'));
 
 COMMIT;
 
 -- Create a check constraint in the old table (2021-01-01 to 2023-02-01)
-ALTER TABLE fungible_token_events_old ADD CONSTRAINT fungible_token_events_old_check_constraint CHECK (event_index >= 16094592000000000000000000000000000 AND event_index < 16752096000000000000000000000000000) NOT VALID;
+ALTER TABLE fungible_token_events_old ADD CONSTRAINT fungible_token_events_old_check_constraint CHECK (event_index >= fn_timestamp2nanosec(TIMESTAMP '2021-01-01') AND event_index < fn_timestamp2nanosec(TIMESTAMP '2023-02-01')) NOT VALID;
 
 -- Attach the old table as partition with previous data (2021-01-01 to 2023-02-01)
-ALTER TABLE fungible_token_events ATTACH PARTITION fungible_token_events_old FOR VALUES FROM (16094592000000000000000000000000000) TO (16752096000000000000000000000000000);
+ALTER TABLE fungible_token_events ATTACH PARTITION fungible_token_events_old FOR VALUES FROM (fn_timestamp2nanosec(TIMESTAMP '2021-01-01')) TO (fn_timestamp2nanosec(TIMESTAMP '2023-02-01'));
 
 -- Now we can drop the old table constraint 
 ALTER TABLE fungible_token_events_old DROP CONSTRAINT fungible_token_events_old_check_constraint;
@@ -115,14 +92,51 @@ BEGIN
 END
 $func$;
 
--- 2023-01-01 to 2023-02-01
-SELECT fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202301', 'event_index', 16698528000000000000000000000000000, 16881696000000000000000000000000000);
--- 2022-12-01 to 2023-01-01
-SELECT fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202212', 'event_index', 16698528000000000000000000000000000, 16725312000000000000000000000000000);
--- 2022-11-01 to 2022-12-01
-SELECT fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202211', 'event_index', 16672608000000000000000000000000000, 16698528000000000000000000000000000);
--- 2022-10-01 to 2022-11-01
-SELECT fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202210', 'event_index', 16645824000000000000000000000000000, 16672608000000000000000000000000000);
+
+
+SELECT fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202301', 'event_index', fn_timestamp2nanosec(TIMESTAMP '2023-01-01'), fn_timestamp2nanosec(TIMESTAMP '2023-02-01'));
+SELECT fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202212', 'event_index', fn_timestamp2nanosec(TIMESTAMP '2022-12-01'), fn_timestamp2nanosec(TIMESTAMP '2023-01-01'));
+SELECT fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202211', 'event_index', fn_timestamp2nanosec(TIMESTAMP '2022-11-01'), fn_timestamp2nanosec(TIMESTAMP '2022-12-01'));
+SELECT fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202210', 'event_index', fn_timestamp2nanosec(TIMESTAMP '2022-10-01'), fn_timestamp2nanosec(TIMESTAMP '2022-11-01'));
+SELECT fn_partition_by_range('fungible_token_events', 'fungible_token_events_old', 'fungible_token_events_p202209', 'event_index', fn_timestamp2nanosec(TIMESTAMP '2022-09-01'), fn_timestamp2nanosec(TIMESTAMP '2022-10-01'));
+
 
 -- Vacuum the table
 VACUUM fungible_token_events;
+
+
+WITH original_query as (
+             SELECT
+                 event_index,
+                 involved_account_id,
+                 delta_amount,
+                 cause,
+                 status,
+                 block_timestamp,
+                 block_height
+             FROM coin_events
+             WHERE contract_account_id = 'token.sweat'
+                 AND affected_account_id = 'oracle.sweat'
+                 AND event_index < 16619904000000000000000000000000000
+             ORDER BY event_index desc
+             LIMIT 100
+         ), timestamps as (
+             SELECT
+                 min(block_timestamp) min_block_timestamp,
+                 max(block_timestamp) max_block_timestamp
+             FROM original_query
+         )
+         SELECT
+             event_index,
+             involved_account_id,
+             delta_amount delta_balance,
+             cause,
+             status,
+             block_timestamp block_timestamp_nanos,
+             block_height
+         FROM coin_events, timestamps
+         WHERE contract_account_id = 'token.sweat'
+             AND affected_account_id = 'oracle.sweat'
+             AND block_timestamp >= min_block_timestamp
+             AND block_timestamp <= max_block_timestamp
+         ORDER BY event_index desc
